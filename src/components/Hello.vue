ef9e1fb07ef5512ce74d22915ea9661c75a54af3
fourWoods <224162391@qq.com>
2017-09-07 23:45:16 +0800

    mall

---
+++ src/components/Hello.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<!--<Counter num='10'></Counter>-->
+		<Counter v-bind:num='num' v-on:incre='addbtn' v-on:decre='decrease'></Counter>
+		<p>{{num}}</p>
+		
+	</div>
+</template>
+
+<script>
+import Counter from './couter'
+export default {
+  name: 'hello',
+  data () {
+    return {
+    	num:'20',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+  	Counter
+  },
+  methods:{
+  	addbtn(){
+  		this.num++
+  	},
+  	decrease(){
+  		this.num--
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/couter.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<button v-on:click="addbtn()">+</button>
+		<button @click="decrease">-</button>
+		<p>{{num}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+	props:['num'],
+  	data () {
+   		return {
+   			num:0
+    	}
+  	},
+  	methods:{
+  		addbtn(){
+  			this.$emit('incre')
+  		},
+  		decrease(){
+  			this.$emit('decre')
+  		}
+  	}
+}
+</script>
+
+<style>
+</style>
